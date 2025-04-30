7e8414d94efc4028eecc2f1a7bdafe25ebe419e7
adarista012 <ad.arista012@gmail.com>
2025-04-30 00:27:14 -0400

    architecture: add clean architecture and bloc patron

---
+++ lib/app/presentation/counter/view/counter_view.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:seek_qr_reader/app/counter/bloc/counter_bloc.dart';
+import 'package:seek_qr_reader/app/counter/bloc/counter_event.dart';
+import 'package:seek_qr_reader/app/counter/bloc/counter_state.dart';
+
+class CounterView extends StatelessWidget {
+  const CounterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      body: Center(
+        child: BlocBuilder<CounterBloc, CounterState>(
+          builder: (_, state) {
+            return Text('${state.count}', style: textTheme.displayMedium);
+          },
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            key: const Key('counterView_increment_floatingActionButton'),
+            child: const Icon(Icons.add),
+            onPressed:
+                () =>
+                    context.read<CounterBloc>().add(CounterIncrementPressed()),
+          ),
+          const SizedBox(height: 8),
+          FloatingActionButton(
+            key: const Key('counterView_decrement_floatingActionButton'),
+            child: const Icon(Icons.remove),
+            onPressed:
+                () =>
+                    context.read<CounterBloc>().add(CounterDecrementPressed()),
+          ),
+          const SizedBox(height: 8),
+          FloatingActionButton(
+            key: const Key('counterView_reset_floatingActionButton'),
+            child: const Icon(Icons.remove),
+            onPressed:
+                () => context.read<CounterBloc>().add(CounterReset(value: 5)),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:seek_qr_reader/app/app.dart';
 
 void main() {
-  runApp(const App());
+  runApp(CounterApp());
 }
